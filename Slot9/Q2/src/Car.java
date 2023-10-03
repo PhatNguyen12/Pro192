3aea8e141fdf3fec2a4366c6c0fedb6ac4c90a23
PhatNguyen12 <144119159+PhatNguyen12@users.noreply.github.com>
2023-10-03 23:21:36 +0700

    Add files via upload

---
+++ Slot9/Q2/src/Car.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author My Computer
+ */
+public class Car {
+    String maker;
+    int price;
+
+    public Car() {
+    }
+
+    public Car(String maker, int price) {
+        this.maker = maker;
+        this.price = price;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Car{" + "maker=" + maker + ", price=" + price + '}';
+    }
+    
+}
